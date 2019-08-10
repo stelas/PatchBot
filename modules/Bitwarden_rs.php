df52860488d32b6a1fad81fa56550ca4d4dd5bc7
Steffen Lange <github@stelas.de>
2019-08-10 10:20:39 +0200

    Add Bitwarden_rs

---
+++ modules/Bitwarden_rs.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bitwarden_rs extends PatchBase {
+	function __construct() {
+		parent::__construct('Daniel García', 'Bitwarden in Rust', 'https://github.com/dani-garcia/bitwarden_rs');
+	}
+	function check() : bool {
+		if ($this->fetch('https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest', true))
+			return $this->parse_json('tag_name');
+		return false;
+	}
+}
+
+?>
