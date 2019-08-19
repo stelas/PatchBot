153b86bf4ea0bc0ab70b42344321719e79e22d4f
Steffen Lange <github@stelas.de>
2019-08-19 14:00:45 +0200

    Add Grafana

---
+++ modules/Grafana.php
@@ -0,0 +1,14 @@
+<?php
+
+class Grafana extends PatchBase {
+	function __construct() {
+		parent::__construct('Grafana Labs', 'Grafana', 'https://grafana.com/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://api.github.com/repos/grafana/grafana/releases/latest', true))
+			return $this->parse_json('tag_name');
+		return false;
+	}
+}
+
+?>
