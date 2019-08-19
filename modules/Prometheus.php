da62ddcc925a8988885ed4493412e3a5519b0ba4
Steffen Lange <github@stelas.de>
2019-08-19 14:01:00 +0200

    Add Prometheus

---
+++ modules/Prometheus.php
@@ -0,0 +1,14 @@
+<?php
+
+class Prometheus extends PatchBase {
+	function __construct() {
+		parent::__construct('Prometheus Authors', 'Prometheus', 'https://prometheus.io/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://api.github.com/repos/prometheus/prometheus/releases/latest', true))
+			return $this->parse_json('tag_name');
+		return false;
+	}
+}
+
+?>
