fd8773f3ecd8a4dcd67ecedbae019366fa6b94c0
Steffen Lange <github@stelas.de>
2020-01-02 17:19:57 +0100

    Add WordPress

---
+++ modules/Wordpress.php
@@ -0,0 +1,14 @@
+<?php
+
+class Wordpress extends PatchBase {
+	function __construct() {
+		parent::__construct('WordPress Foundation', 'WordPress', 'https://wordpress.org/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://wordpress.org/download/'))
+			return $this->parse('/Download WordPress ([\d\.]+)/');
+		return false;
+	}
+}
+
+?>
