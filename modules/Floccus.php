2b6c4f1b8746bdf85bc28dff4ae0d137588cb0d3
Steffen Lange <github@stelas.de>
2020-01-28 15:36:51 +0100

    Add floccus

---
+++ modules/Floccus.php
@@ -0,0 +1,14 @@
+<?php
+
+class Floccus extends PatchBase {
+	function __construct() {
+		parent::__construct('Marcel Klehr', 'floccus', 'https://addons.mozilla.org/en-US/firefox/addon/floccus/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://addons.mozilla.org/en-US/firefox/addon/floccus/'))
+			return $this->parse('/"version":"([\d\.]+)"/');
+		return false;
+	}
+}
+
+?>
