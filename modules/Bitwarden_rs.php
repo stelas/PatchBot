91333c48bc3cde26b8f1aab18431b2acd15eadfd
Steffen Lange <github@stelas.de>
2020-02-24 15:43:05 +0100

    Bitwarden Web Vault: Correct name and link

--- modules/Bitwarden_rs.php
+++ modules/Bitwarden_rs.php
@@ -2,7 +2,7 @@
 
 class Bitwarden_rs extends PatchBase {
 	function __construct() {
-		parent::__construct('Daniel García', 'Bitwarden in Rust', 'https://github.com/dani-garcia/bitwarden_rs');
+		parent::__construct('Daniel García', 'Bitwarden Web Vault in Rust', 'https://github.com/dani-garcia/bw_web_builds');
 	}
 	function check() : bool {
 		if ($this->fetch('https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest', true))
