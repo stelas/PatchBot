03928bb43e301817842823f2a061f4dc09a49ba1
Steffen Lange <github@stelas.de>
2020-04-05 16:20:54 +0200

    Add jQuery

---
+++ modules/JQuery.php
@@ -0,0 +1,14 @@
+<?php
+
+class JQuery extends PatchBase {
+	function __construct() {
+		parent::__construct('jQuery Foundation', 'jQuery', 'https://jquery.com/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://api.github.com/repos/jquery/jquery/tags', true))
+			return $this->parse_json('name');
+		return false;
+	}
+}
+
+?>
