6eebe2e3b65dc3d2481a56b857447ae06eb04b34
Steffen Lange <github@stelas.de>
2020-04-22 12:01:21 +0200

    Add Inkscape

---
+++ modules/Inkscape.php
@@ -0,0 +1,14 @@
+<?php
+
+class Inkscape extends PatchBase {
+	function __construct() {
+		parent::__construct('Inkscape Authors', 'Inkscape', 'https://inkscape.org/release/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://inkscape.org/release/'))
+			return $this->parse('/<title>Download Inkscape ([\d\.]+)/');
+		return false;
+	}
+}
+
+?>
