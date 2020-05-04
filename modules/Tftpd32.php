228881763edf97f98d25759aeac6da804966ab72
Steffen Lange <github@stelas.de>
2020-05-04 15:21:29 +0200

    Tftpd32: Rename class

--- modules/Tftpd32.php
+++ modules/Tftpd32.php
@@ -1,8 +1,8 @@
 <?php
 
-class TFTPD32 extends PatchBase {
+class Tftpd32 extends PatchBase {
 	function __construct() {
-		parent::__construct('Philippe Jounin', 'TFTPD64', 'https://tftpd32.jounin.net/tftpd32_download.html');
+		parent::__construct('Philippe Jounin', 'Tftpd64', 'https://tftpd32.jounin.net/tftpd32_download.html');
 	}
 	function check() : bool {
 		if ($this->fetch('https://tftpd32.jounin.net/tftpd32_news.html'))
