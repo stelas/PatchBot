5e8f240348734175fcb828bc136133ca008d943a
Steffen Lange <github@stelas.de>
2020-05-11 10:05:16 +0200

    GCLog: Full product name

--- modules/GCLog.php
+++ modules/GCLog.php
@@ -2,7 +2,7 @@
 
 class GCLog extends PatchBase {
 	function __construct() {
-		parent::__construct('Steffen Lange', 'GCLog', 'https://www.gclog.de/');
+		parent::__construct('Steffen Lange', 'Geiger Counter Logger', 'https://www.gclog.de/');
 	}
 	function check() : bool {
 		if ($this->fetch('https://api.github.com/repos/stelas/GCLog/tags', true))
