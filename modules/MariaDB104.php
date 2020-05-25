167e31e50e9c1424b689a88e2573fe7b5b428e48
Steffen Lange <github@stelas.de>
2020-05-25 15:55:42 +0200

    Add MariaDB 10.4

---
+++ modules/MariaDB104.php
@@ -0,0 +1,15 @@
+<?php
+
+class MariaDB104 extends PatchBase {
+	function __construct() {
+		parent::__construct('MariaDB Foundation', 'MariaDB', 'https://downloads.mariadb.org/');
+		$this->patch->setBranch('10.4');
+	}
+	function check() : bool {
+		if ($this->fetch('https://downloads.mariadb.com/MariaDB/mariadb-10.4/source/'))
+			return $this->parse('/mariadb-([\d\.]+)\.tar\.gz/');
+		return false;
+	}
+}
+
+?>
