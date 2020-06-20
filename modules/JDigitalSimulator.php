b05b5cdd5d5dd83d814caa3881b5fa04f70bc4cf
Steffen Lange <github@stelas.de>
2020-06-20 11:46:35 +0200

    JDigitalSimulator: fix typo

--- modules/JDigitalSimulator.php
+++ modules/JDigitalSimulator.php
@@ -2,7 +2,7 @@
 
 class JDigitalSimulator extends PatchBase {
 	function __construct() {
-		parent::__construct('Kristian Kraljic', 'JDigitalSimualtor', 'https://kra.lc/projects/jdigitalsimulator/download.html');
+		parent::__construct('Kristian Kraljic', 'JDigitalSimulator', 'https://kra.lc/projects/jdigitalsimulator/download.html');
 	}
 	function check() : bool {
 		if ($this->fetch('https://api.github.com/repos/kristian/JDigitalSimulator/releases/latest', true))
