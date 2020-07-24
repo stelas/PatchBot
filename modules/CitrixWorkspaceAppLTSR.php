264f0e6d64398d42ee0e681cd61eee2604a1a721
Steffen Lange <github@stelas.de>
2020-07-25 00:17:55 +0200

    Add Citrix Workspace app LTSR

---
+++ modules/CitrixWorkspaceAppLTSR.php
@@ -0,0 +1,15 @@
+<?php
+
+class CitrixWorkspaceAppLTSR extends PatchBase {
+	function __construct() {
+		parent::__construct('Citrix', 'Workspace App', 'https://www.citrix.com/downloads/workspace-app/workspace-app-for-windows-long-term-service-release/');
+		$this->patch->setBranch('LTSR');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.citrix.com/downloads/workspace-app/workspace-app-for-windows-long-term-service-release/'))
+			return $this->parse('/>Citrix Workspace app ([\d\.]+).+LTSR/');
+		return false;
+	}
+}
+
+?>
