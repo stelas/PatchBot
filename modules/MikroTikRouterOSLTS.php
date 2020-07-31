43c2f8baf7b150ccc2b3ea8722023c1539bcdae1
Steffen Lange <github@stelas.de>
2020-07-31 11:39:05 +0200

    Add MikroTik RouterOS Long-term release

---
+++ modules/MikroTikRouterOSLTS.php
@@ -0,0 +1,17 @@
+<?php
+
+class MikroTikRouterOSLTS extends PatchBase {
+	function __construct() {
+		parent::__construct('MikroTik', 'RouterOS', 'https://mikrotik.com/download');
+		$this->patch->setBranch('Long-term');
+	}
+	function check() : bool {
+		if ($this->fetch('https://mikrotik.com/download')) {
+			$this->str_crop('id="routeros"', '</thead>');
+			return $this->parse('/<th>([\d\.]+) \(Long-term\)/');
+		}
+		return false;
+	}
+}
+
+?>
