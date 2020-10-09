ae0b34362efe4ea3ad24ed163211c8ea97756842
Steffen Lange <github@stelas.de>
2020-10-09 21:32:28 +0200

    Add TreeSheets

---
+++ modules/TreeSheets.php
@@ -0,0 +1,15 @@
+<?php
+
+class TreeSheets extends PatchBase {
+	function __construct() {
+		parent::__construct('Wouter van Oortmerssen', 'TreeSheets', 'http://strlen.com/treesheets/');
+		$this->patch->setBranch('Continuous');
+	}
+	function check() : bool {
+		if ($this->fetch('https://api.github.com/repos/aardappel/treesheets/releases/latest', true))
+			return $this->parse_json('target_commitish', '/(.{7})/');
+		return false;
+	}
+}
+
+?>
