6c2f34326336e6ee1cde2d1c39154eeb698e654c
Steffen Lange <github@stelas.de>
2020-12-16 21:38:37 +0100

    Add lazydocker

---
+++ modules/Lazydocker.php
@@ -0,0 +1,14 @@
+<?php
+
+class Lazydocker extends PatchBase {
+	function __construct() {
+		parent::__construct('Jesse Duffield', 'lazydocker', 'https://github.com/jesseduffield/lazydocker');
+	}
+	function check() : bool {
+		if ($this->fetch('https://api.github.com/repos/jesseduffield/lazydocker/releases/latest', true))
+			return $this->parse_json('tag_name');
+		return false;
+	}
+}
+
+?>
