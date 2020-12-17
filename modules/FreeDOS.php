762967f65495dd7a66bbdfa121563235455a8f9e
Steffen Lange <github@stelas.de>
2020-12-17 17:45:03 +0100

    Add FreeDOS

---
+++ modules/FreeDOS.php
@@ -0,0 +1,14 @@
+<?php
+
+class FreeDOS extends PatchBase {
+	function __construct() {
+		parent::__construct('Jim Hall & FreeDOS Team', 'FreeDOS', 'https://www.freedos.org/download/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.freedos.org/download/'))
+			return $this->parse('/<h2>FreeDOS ([\d\.]+)<\/h2>/');
+		return false;
+	}
+}
+
+?>
