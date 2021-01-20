6bcafa1e0104385fec732497cb68969e15d408cb
Steffen Lange <github@stelas.de>
2021-01-20 15:37:04 +0100

    Add VeraCrypt

---
+++ modules/VeraCrypt.php
@@ -0,0 +1,14 @@
+<?php
+
+class VeraCrypt extends PatchBase {
+	function __construct() {
+		parent::__construct('IDRIX', 'VeraCrypt', 'https://www.veracrypt.fr/en/Downloads.html');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.veracrypt.fr/en/Downloads.html'))
+			return $this->parse('_//launchpad\.net/veracrypt/trunk/[\d\.]+[a-z]?[-a-z\d]*/&#43;download/VeraCrypt%20Setup%20([\d\.]+[a-z]?[-A-Za-z\d]*)\.exe_');
+		return false;
+	}
+}
+
+?>
