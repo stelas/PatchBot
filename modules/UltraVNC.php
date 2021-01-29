ce92c44b489e21f9fa23d1769249a01da127a841
Steffen Lange <github@stelas.de>
2021-01-29 13:07:58 +0100

    UltraVNC: fix regex again

--- modules/UltraVNC.php
+++ modules/UltraVNC.php
@@ -5,8 +5,10 @@ class UltraVNC extends PatchBase {
 		parent::__construct('UltraVNC Team', 'UltraVNC', 'https://www.uvnc.com/downloads/ultravnc.html');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.uvnc.com/downloads/ultravnc.html'))
-			return $this->parse('/[Dd]ownload [Uu]ltra[Vv][Nn][Cc] ([\d\.]+)/');
+		if ($this->fetch('https://www.uvnc.com/downloads/ultravnc.html')) {
+			$this->data = utf8_decode($this->data);
+			return $this->parse('/[Ll]atest.[Vv]ersion.+[Rr]elease.([\d\.]+)/');
+		}
 		return false;
 	}
 }
