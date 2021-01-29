d2bbd24b5f9f7536e2a32caff2dcd3f06126fcb7
Steffen Lange <github@stelas.de>
2021-01-29 18:28:05 +0100

    UltraVNC: use unicode support in re

--- modules/UltraVNC.php
+++ modules/UltraVNC.php
@@ -5,10 +5,8 @@ class UltraVNC extends PatchBase {
 		parent::__construct('UltraVNC Team', 'UltraVNC', 'https://www.uvnc.com/downloads/ultravnc.html');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.uvnc.com/downloads/ultravnc.html')) {
-			$this->data = utf8_decode($this->data);
-			return $this->parse('/[Ll]atest.[Vv]ersion.+[Rr]elease.([\d\.]+)/');
-		}
+		if ($this->fetch('https://www.uvnc.com/downloads/ultravnc.html'))
+			return $this->parse('/[Ll]atest.[Vv]ersion.+[Rr]elease.([\d\.]+)/u');
 		return false;
 	}
 }
