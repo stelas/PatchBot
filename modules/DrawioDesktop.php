d32f900c8bd7205959c63cbe762eb92ce9f771cb
Steffen Lange <github@stelas.de>
2021-05-14 23:26:24 +0200

    Add draw.io Desktop

---
+++ modules/DrawioDesktop.php
@@ -0,0 +1,15 @@
+<?php
+
+class DrawioDesktop extends PatchBase {
+	function __construct() {
+		parent::__construct('JGraph Ltd', 'draw.io', 'https://www.diagrams.net/');
+		$this->patch->setBranch('Desktop');
+	}
+	function check() : bool {
+		if ($this->fetch_json('https://api.github.com/repos/jgraph/drawio-desktop/releases/latest'))
+			return $this->parse_json('name');
+		return false;
+	}
+}
+
+?>
