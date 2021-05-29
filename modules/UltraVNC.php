311846b4c98796df938db55513ce89804b38b815
Steffen Lange <github@stelas.de>
2021-05-30 00:15:48 +0200

    UVNC: fix

--- modules/UltraVNC.php
+++ modules/UltraVNC.php
@@ -6,7 +6,7 @@ class UltraVNC extends PatchBase {
 	}
 	function check() : bool {
 		if ($this->fetch('https://www.uvnc.com/downloads/ultravnc.html'))
-			return $this->parse('/[Ll]atest.[Vv]ersion.+[Rr]elease.([\d\.]+)/u');
+			return $this->parse('/[Ll]atest.[Rr]elease.[Vv]ersion.+[Rr]elease.([\d\.]+)/u');
 		return false;
 	}
 }
