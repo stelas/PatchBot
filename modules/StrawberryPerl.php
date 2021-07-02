e1ce0b18017b3142ed73f30b367baa01a1f6d41a
Steffen Lange <github@stelas.de>
2021-07-02 10:35:27 +0200

    Add Strawberry Perl

---
+++ modules/StrawberryPerl.php
@@ -0,0 +1,14 @@
+<?php
+
+class StrawberryPerl extends PatchBase {
+	function __construct() {
+		parent::__construct('Adam Kennedy', 'Strawberry Perl', 'https://strawberryperl.com/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://strawberryperl.com/'))
+			return $this->parse('_/download/[\d\.]+/strawberry-perl-([\d\.]+)-\d+bit\.msi_');
+		return false;
+	}
+}
+
+?>
