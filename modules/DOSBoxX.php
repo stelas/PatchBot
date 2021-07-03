8a091ae484b6a2d1d43169c095be89b944930a72
Steffen Lange <github@stelas.de>
2021-07-03 11:40:46 +0200

    Add DOSBox-X

---
+++ modules/DOSBoxX.php
@@ -0,0 +1,14 @@
+<?php
+
+class DOSBoxX extends PatchBase {
+	function __construct() {
+		parent::__construct('Jonathan Campbell', 'DOSBox-X', 'https://dosbox-x.com/');
+	}
+	function check() : bool {
+		if ($this->fetch_json('https://api.github.com/repos/joncampbell123/dosbox-x/releases/latest'))
+			return $this->parse_json('tag_name', '/dosbox-x-v(.+)/');
+		return false;
+	}
+}
+
+?>
