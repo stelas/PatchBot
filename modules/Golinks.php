32d0ac744ea45b45b7a15fce321d6a138b963a2b
Steffen Lange <github@stelas.de>
2021-07-27 15:11:14 +0200

    Golinks: Repo moved to git.mills.io

--- modules/Golinks.php
+++ modules/Golinks.php
@@ -2,10 +2,10 @@
 
 class Golinks extends PatchBase {
 	function __construct() {
-		parent::__construct('James Mills', 'golinks', 'https://prologic.github.io/golinks/');
+		parent::__construct('James Mills', 'golinks', 'https://git.mills.io/prologic/golinks');
 	}
 	function check() : bool {
-		if ($this->fetch_json('https://api.github.com/repos/prologic/golinks/releases/latest'))
+		if ($this->fetch_json('https://git.mills.io/api/v1/repos/prologic/golinks/releases?pre-release=false&limit=1'))
 			return $this->parse_json('tag_name');
 		return false;
 	}
