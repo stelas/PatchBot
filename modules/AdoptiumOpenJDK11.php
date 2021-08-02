7bd8f9c7c38d2766ea14d3423eda6ac766e0d475
Steffen Lange <github@stelas.de>
2021-08-02 16:40:21 +0200

    Rename Adoptium OpenJDK

--- modules/AdoptiumOpenJDK11.php
+++ modules/AdoptiumOpenJDK11.php
@@ -2,8 +2,8 @@
 
 class AdoptiumOpenJDK11 extends PatchBase {
 	function __construct() {
-		parent::__construct('Adoptium', 'OpenJDK', 'https://adoptium.net/releases.html?variant=openjdk11&jvmVariant=hotspot');
-		$this->patch->setBranch('11');
+		parent::__construct('Eclipse Foundation', 'Adoptium OpenJDK', 'https://adoptium.net/releases.html?variant=openjdk11&jvmVariant=hotspot');
+		$this->patch->setBranch('Temurin 11');
 	}
 	function check() : bool {
 		if ($this->fetch_json('https://api.adoptium.net/v3/info/release_versions?heap_size=normal&image_type=jdk&page=0&page_size=10&project=jdk&release_type=ga&sort_method=DEFAULT&sort_order=DESC&vendor=adoptium&version=%5B11.0%2C11.1%29'))
--- modules/AdoptiumOpenJDK8.php
+++ modules/AdoptiumOpenJDK8.php
@@ -2,8 +2,8 @@
 
 class AdoptiumOpenJDK8 extends PatchBase {
 	function __construct() {
-		parent::__construct('Adoptium', 'OpenJDK', 'https://adoptium.net/releases.html?variant=openjdk8&jvmVariant=hotspot');
-		$this->patch->setBranch('8');
+		parent::__construct('Eclipse Foundation', 'Adoptium OpenJDK', 'https://adoptium.net/releases.html?variant=openjdk8&jvmVariant=hotspot');
+		$this->patch->setBranch('Temurin 8');
 	}
 	function check() : bool {
 		if ($this->fetch_json('https://api.adoptium.net/v3/info/release_versions?heap_size=normal&image_type=jdk&page=0&page_size=10&project=jdk&release_type=ga&sort_method=DEFAULT&sort_order=DESC&vendor=adoptium&version=%5B8.0%2C8.1%29'))
