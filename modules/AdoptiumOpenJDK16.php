f890ff9d478b9bc462b7daff9dd3a7226fdeafd6
Steffen Lange <github@stelas.de>
2021-08-02 15:30:03 +0200

    Add Adoptium OpenJDK

---
+++ modules/AdoptiumOpenJDK16.php
@@ -0,0 +1,15 @@
+<?php
+
+class AdoptiumOpenJDK16 extends PatchBase {
+	function __construct() {
+		parent::__construct('Adoptium', 'OpenJDK', 'https://adoptium.net/releases.html?variant=openjdk16&jvmVariant=hotspot');
+		$this->patch->setBranch('16');
+	}
+	function check() : bool {
+		if ($this->fetch_json('https://api.adoptium.net/v3/info/release_versions?heap_size=normal&image_type=jdk&page=0&page_size=10&project=jdk&release_type=ga&sort_method=DEFAULT&sort_order=DESC&vendor=adoptium&version=%5B16.0%2C16.1%29'))
+			return $this->parse_json('openjdk_version');
+		return false;
+	}
+}
+
+?>
