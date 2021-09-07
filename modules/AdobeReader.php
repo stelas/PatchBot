6e76d32b8e3710a2cf51126a4d9f06e2ec00a904
Steffen Lange <github@stelas.de>
2021-09-07 15:40:09 +0200

    AdobeReader: update regex

--- modules/AdobeReader.php
+++ modules/AdobeReader.php
@@ -7,7 +7,7 @@ class AdobeReader extends PatchBase {
 	}
 	function check() : bool {
 		if ($this->fetch('https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html'))
-			return $this->parse('/next: ([\d\.]+)/');
+			return $this->parse('/rel="next" title="([\d\.]+)/');
 		return false;
 	}
 }
