0f6944655ae074ca47516f371f36b34dbf0b23fb
Steffen Lange <github@stelas.de>
2021-09-29 01:56:40 +0200

    Add Adoptium OpenJDK 17

---
+++ modules/AdoptiumOpenJDK17.php
@@ -0,0 +1,15 @@
+<?php
+
+class AdoptiumOpenJDK17 extends PatchBase {
+	function __construct() {
+		parent::__construct('Eclipse Foundation', 'Adoptium OpenJDK', 'https://adoptium.net/releases.html?variant=openjdk17&jvmVariant=hotspot');
+		$this->patch->setBranch('Temurin 17');
+	}
+	function check() : bool {
+		if ($this->fetch_json('https://api.adoptium.net/v3/info/release_versions?heap_size=normal&image_type=jdk&page=0&page_size=10&project=jdk&release_type=ga&sort_method=DEFAULT&sort_order=DESC&vendor=adoptium&version=%5B17.0%2C17.1%29'))
+			return $this->parse_json('openjdk_version');
+		return false;
+	}
+}
+
+?>
