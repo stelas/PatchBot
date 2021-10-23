3d925c1cd1ee96d6d6267ea34c97334281e0d3d3
Steffen Lange <github@stelas.de>
2021-10-24 00:27:34 +0200

    MariaDB 10.6: use REST API

--- modules/MariaDB106.php
+++ modules/MariaDB106.php
@@ -2,12 +2,12 @@
 
 class MariaDB106 extends PatchBase {
 	function __construct() {
-		parent::__construct('MariaDB Foundation', 'MariaDB', 'https://downloads.mariadb.org/mariadb/10.6/');
+		parent::__construct('MariaDB Foundation', 'MariaDB', 'https://mariadb.org/download/');
 		$this->patch->setBranch('10.6');
 	}
 	function check() : bool {
-		if ($this->fetch('https://downloads.mariadb.org/mariadb/10.6/'))
-			return $this->parse('/MariaDB ([\d\.]+) Stable/');
+		if ($this->fetch_json('https://downloads.mariadb.org/rest-api/mariadb/10.6/'))
+			return $this->parse_json('release_id');
 		return false;
 	}
 }
