80728298cbf443eae533aeb062cd309234bddecb
Steffen Lange <github@stelas.de>
2021-10-29 13:20:45 +0200

    Add Memtest86+

---
+++ modules/Memtest.php
@@ -0,0 +1,14 @@
+<?php
+
+class Memtest extends PatchBase {
+	function __construct() {
+		parent::__construct('Samuel Demeulemeester', 'Memtest86+', 'https://www.memtest.org/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.memtest.org/'))
+			return $this->parse('/>LATEST\s+VERSION : ([\d\.]+[a-z]?)</');
+		return false;
+	}
+}
+
+?>
