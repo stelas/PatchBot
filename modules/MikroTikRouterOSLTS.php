7d41c13668e5b2fdbb014c1669005c8d1a7ca0fe
Steffen Lange <github@stelas.de>
2021-12-13 15:56:23 +0100

    RouterOS Long-term: fix regex

--- modules/MikroTikRouterOSLTS.php
+++ modules/MikroTikRouterOSLTS.php
@@ -8,7 +8,7 @@ class MikroTikRouterOSLTS extends PatchBase {
 	function check() : bool {
 		if ($this->fetch('https://mikrotik.com/download')) {
 			$this->str_crop('id="routeros"', '</thead>');
-			return $this->parse('/<th>([\d\.]+) \(Long-term\)/');
+			return $this->parse('/<th>([\d\.]+) Long-term<\/th>/');
 		}
 		return false;
 	}
