33481e4a3685a87cf24f81c608d41b7b4abfddcc
Steffen Lange <github@stelas.de>
2021-12-13 15:56:05 +0100

    RouterOS: fix regex

--- modules/MikroTikRouterOS.php
+++ modules/MikroTikRouterOS.php
@@ -8,7 +8,7 @@ class MikroTikRouterOS extends PatchBase {
 	function check() : bool {
 		if ($this->fetch('https://mikrotik.com/download')) {
 			$this->str_crop('id="routeros"', '</thead>');
-			return $this->parse('/<th>([\d\.]+) \(Stable\)/');
+			return $this->parse('/<th>([\d\.]+) Stable<\/th>/');
 		}
 		return false;
 	}
