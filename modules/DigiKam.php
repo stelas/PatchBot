52d5074d1c521c1d853ba31891845a26237dad7b
Steffen Lange <github@stelas.de>
2021-12-14 13:18:25 +0100

    Add digiKam

---
+++ modules/DigiKam.php
@@ -0,0 +1,14 @@
+<?php
+
+class DigiKam extends PatchBase {
+	function __construct() {
+		parent::__construct('digiKam Team', 'digiKam', 'https://www.digikam.org/download/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.digikam.org/download/'))
+			return $this->parse('_//download\.kde\.org/stable/digikam/[\d\.]+/digiKam-([\d\.]+)-Win64\.exe\.mirrorlist_');
+		return false;
+	}
+}
+
+?>
