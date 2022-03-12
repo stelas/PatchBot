ddd5202264e34fcf232fc5be8ff8e0d3456d1def
Steffen Lange <github@stelas.de>
2022-03-12 22:27:45 +0100

    SystemRescue: new URL

--- modules/SysRescCd.php
+++ modules/SysRescCd.php
@@ -2,10 +2,10 @@
 
 class SysRescCd extends PatchBase {
 	function __construct() {
-		parent::__construct('Francois Dupoux', 'SystemRescueCd', 'http://www.system-rescue-cd.org/Download/');
+		parent::__construct('Francois Dupoux', 'SystemRescueCd', 'http://www.system-rescue.org/Download/');
 	}
 	function check() : bool {
-		if ($this->fetch('http://www.system-rescue-cd.org/Detailed-packages-list/'))
+		if ($this->fetch('http://www.system-rescue.org/Detailed-packages-list/'))
 			return $this->parse('/<strong>SystemRescue-([\d\.]+)<\/strong>/');
 		return false;
 	}
