83d5ac8517522424a2961f459f8f47f8d52230cd
Steffen Lange <github@stelas.de>
2022-04-30 15:49:16 +0200

    PatchCollector: exit with return code on db failure

--- PatchCollector.php
+++ PatchCollector.php
@@ -18,24 +18,26 @@ foreach (new \DirectoryIterator(__DIR__ . '/modules') as $file) {
 
 $db = new Database(__DIR__ . '/db.json');
 echo 'Time: ' . date('c', $db->time()) . PHP_EOL;
-if ($db->load()) {
-	foreach ($list as $patch) {
-		$oldVer = $db->find($patch->id())->getVersion();
-		if ($patch->check()) {
-			$newVer = $patch->getPatch()->getVersion();
-			echo $patch->id() . ': Version \'' . $newVer . '\' is ';
-			if (!empty($newVer) && $newVer != $oldVer) {
-				echo "\033[32m" . 'newer than \'' . $oldVer . '\'' . "\033[39m";
-				$db->addOrUpdate($patch->getPatch());
-			}
-			else
-				echo "\033[90m" . 'up to date' . "\033[39m";
-			echo '.' . PHP_EOL;
+if (!$db->load()) {
+	exit(1);
+}
+
+foreach ($list as $patch) {
+	$oldVer = $db->find($patch->id())->getVersion();
+	if ($patch->check()) {
+		$newVer = $patch->getPatch()->getVersion();
+		echo $patch->id() . ': Version \'' . $newVer . '\' is ';
+		if (!empty($newVer) && $newVer != $oldVer) {
+			echo "\033[32m" . 'newer than \'' . $oldVer . '\'' . "\033[39m";
+			$db->addOrUpdate($patch->getPatch());
 		}
 		else
-			fwrite(STDERR, $patch->id() . ': ' . "\033[31m" . 'CHECK FAILED' . "\033[39m" . '!' . PHP_EOL);
+			echo "\033[90m" . 'up to date' . "\033[39m";
+		echo '.' . PHP_EOL;
 	}
-	$db->save();
+	else
+		fwrite(STDERR, $patch->id() . ': ' . "\033[31m" . 'CHECK FAILED' . "\033[39m" . '!' . PHP_EOL);
 }
+$db->save();
 
 ?>
