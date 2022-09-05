9d40a5f595c1c67b9de3ff90480e6cf1245011ae
Steffen Lange <git@stelas.de>
2022-09-05 15:22:14 +0200

    GIMP: download path changed

--- modules/GIMP.php
+++ modules/GIMP.php
@@ -6,7 +6,7 @@ class GIMP extends PatchBase {
 	}
 	function check() : bool {
 		if ($this->fetch('https://www.gimp.org/downloads/'))
-			return $this->parse('_//download\.gimp\.org/mirror/pub/gimp/v[\d\.]+/windows/gimp-([\d\.]+)-setup[-\d]*\.exe_');
+			return $this->parse('_//download\.gimp\.org/gimp/v[\d\.]+/windows/gimp-([\d\.]+)-setup[-\d]*\.exe_');
 		return false;
 	}
 }
