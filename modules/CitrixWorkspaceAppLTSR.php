c2db06aef733e05544f95f72c996daa8b79b33dc
Steffen Lange <git@stelas.de>
2022-12-02 10:36:14 +0100

    Workspace App LTSR: update url

--- modules/CitrixWorkspaceAppLTSR.php
+++ modules/CitrixWorkspaceAppLTSR.php
@@ -2,11 +2,11 @@
 
 class CitrixWorkspaceAppLTSR extends PatchBase {
 	function __construct() {
-		parent::__construct('Citrix', 'Workspace App', 'https://www.citrix.com/downloads/workspace-app/workspace-app-for-windows-long-term-service-release/workspace-app-for-windows-LTSR.html');
+		parent::__construct('Citrix', 'Workspace App', 'https://www.citrix.com/downloads/workspace-app/workspace-app-for-windows-long-term-service-release/workspace-app-for-windows-LTSR-Latest.html');
 		$this->patch->setBranch('LTSR');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.citrix.com/downloads/workspace-app/workspace-app-for-windows-long-term-service-release/workspace-app-for-windows-LTSR.html'))
+		if ($this->fetch('https://www.citrix.com/downloads/workspace-app/workspace-app-for-windows-long-term-service-release/workspace-app-for-windows-LTSR-Latest.html'))
 			return $this->parse('/<p>Version: ([\d\.]+)<\/p>/');
 		return false;
 	}
