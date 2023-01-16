69c10a3ac944d88f47caab4c4295981df138e856
Steffen Lange <git@stelas.de>
2023-01-16 17:17:11 +0100

    Add OPNsense

---
+++ modules/OPNsense.php
@@ -0,0 +1,14 @@
+<?php
+
+class OPNsense extends PatchBase {
+	function __construct() {
+		parent::__construct('Deciso B.V.', 'OPNsense', 'https://opnsense.org/download/');
+	}
+	function check() : bool {
+		if ($this->fetch('https://opnsense.org/download/'))
+			return $this->parse('/version *= *"([\d.]+)"/');
+		return false;
+	}
+}
+
+?>
