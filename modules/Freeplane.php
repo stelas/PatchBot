1088a9b19d2aaac089e7ab5ea17b744ceb189cfe
Steffen Lange <git@stelas.de>
2023-01-18 13:15:47 +0100

    Freeplane: regex for update releases enhanced

--- modules/Freeplane.php
+++ modules/Freeplane.php
@@ -6,7 +6,7 @@ class Freeplane extends PatchBase {
 	}
 	function check() : bool {
 		if ($this->fetch_json('https://sourceforge.net/projects/freeplane/best_release.json'))
-			return $this->parse_json('filename', '/-([\d\.]+)\.[a-z]+$/');
+			return $this->parse_json('filename', '/-([\d\.]+[u\d]*)\.[a-z]+$/');
 		return false;
 	}
 }
