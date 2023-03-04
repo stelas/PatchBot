3c7fd967d023c092c0608fabd08eb0a5b9725a1f
Steffen Lange <git@stelas.de>
2023-03-04 12:34:16 +0100

    KiCad: use GitHub release tags

--- modules/KiCad.php
+++ modules/KiCad.php
@@ -5,8 +5,8 @@ class KiCad extends PatchBase {
 		parent::__construct('KiCad Developers', 'KiCad', 'https://kicad.org/download/');
 	}
 	function check() : bool {
-		if ($this->fetch('https://kicad.org/download/source/'))
-			return $this->parse('/<p>Current Version: <strong>([\d\.]+)<\/strong><\/p>/');
+		if ($this->fetch_json('https://api.github.com/repos/KiCad/kicad-source-mirror/releases/latest'))
+			return $this->parse_json('tag_name');
 		return false;
 	}
 }
