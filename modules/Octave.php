202df1d1c6954468ef507303bd2184ad689c0c11
Steffen Lange <git@stelas.de>
2023-03-04 12:35:16 +0100

    Octave: Web page has been changed

--- modules/Octave.php
+++ modules/Octave.php
@@ -2,11 +2,11 @@
 
 class Octave extends PatchBase {
 	function __construct() {
-		parent::__construct('John W. Eaton', 'GNU Octave', 'https://www.gnu.org/software/octave/download');
+		parent::__construct('John W. Eaton', 'GNU Octave', 'https://octave.org/download');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.gnu.org/software/octave/download'))
-			return $this->parse('_//ftpmirror\.gnu\.org/octave/windows/octave-([\d\.]+)-w64-installer\.exe_');
+		if ($this->fetch('https://octave.org/download'))
+			return $this->parse('/<strong>GNU Octave ([\d\.]+)<\/strong>/');
 		return false;
 	}
 }
