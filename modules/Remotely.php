6bec73393aa86060fdd3c62885e630c9b1f1cf3c
Steffen Lange <git@stelas.de>
2023-07-08 10:48:18 +0200

    Remotely: update url

--- modules/Remotely.php
+++ modules/Remotely.php
@@ -2,10 +2,10 @@
 
 class Remotely extends PatchBase {
 	function __construct() {
-		parent::__construct('Jared Goodwin', 'Remotely', 'https://remotely.one/');
+		parent::__construct('Jared Goodwin', 'Remotely', 'https://github.com/immense/Remotely');
 	}
 	function check() : bool {
-		if ($this->fetch_json('https://api.github.com/repos/lucent-sea/Remotely/releases/latest'))
+		if ($this->fetch_json('https://api.github.com/repos/immense/Remotely/releases/latest'))
 			return $this->parse_json('tag_name');
 		return false;
 	}
