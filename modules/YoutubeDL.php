328841ef53995ed65c24954c6f3f6c271d0f736b
Steffen Lange <git@stelas.de>
2023-08-11 22:20:30 +0200

    Youtube-DL: New URL thanks to the fools from LG Hamburg

--- modules/YoutubeDL.php
+++ modules/YoutubeDL.php
@@ -2,7 +2,7 @@
 
 class YoutubeDL extends PatchBase {
 	function __construct() {
-		parent::__construct('youtube-dl Authors', 'youtube-dl', 'https://yt-dl.org/');
+		parent::__construct('youtube-dl Authors', 'youtube-dl', 'http://ytdl-org.github.io/youtube-dl/');
 	}
 	function check() : bool {
 		if ($this->fetch_json('https://api.github.com/repos/ytdl-org/youtube-dl/releases/latest'))
