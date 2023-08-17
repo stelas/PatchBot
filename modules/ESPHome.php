1427899bab6ec3f3176f6804696cfe7abaebe66c
Steffen Lange <git@stelas.de>
2023-08-17 11:46:07 +0200

    Add ESPHome

---
+++ modules/ESPHome.php
@@ -0,0 +1,14 @@
+<?php
+
+class ESPHome extends PatchBase {
+	function __construct() {
+		parent::__construct('Jesse Hills', 'ESPHome', 'https://esphome.io/');
+	}
+	function check() : bool {
+		if ($this->fetch_json('https://api.github.com/repos/esphome/esphome/releases/latest'))
+			return $this->parse_json('tag_name');
+		return false;
+	}
+}
+
+?>
