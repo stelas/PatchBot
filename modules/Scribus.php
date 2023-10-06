1dc817532fa182cbcd6bf50e2a0715869e36fc91
Steffen Lange <git@stelas.de>
2023-10-06 11:27:13 +0200

    Scribus: update url and regex

--- modules/Scribus.php
+++ modules/Scribus.php
@@ -5,8 +5,8 @@ class Scribus extends PatchBase {
 		parent::__construct('Scribus Team', 'Scribus', 'https://www.scribus.net/downloads/stable-branch/');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.scribus.net/downloads/stable-branch/'))
-			return $this->parse('/<strong>Current stable release: Scribus ([\d\.]+)<\/strong>/');
+		if ($this->fetch('https://www.scribus.net/downloads/'))
+			return $this->parse('_//sourceforge\.net/projects/scribus/files/scribus/([\d\.]+)/_');
 		return false;
 	}
 }
