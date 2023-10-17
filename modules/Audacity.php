f505201149c34e9f10f091f0c3cdfd11b3af4020
Steffen Lange <git@stelas.de>
2023-10-17 17:02:40 +0200

    Audacity: fix broken regex

--- modules/Audacity.php
+++ modules/Audacity.php
@@ -6,7 +6,7 @@ class Audacity extends PatchBase {
 	}
 	function check() : bool {
 		if ($this->fetch('https://www.audacityteam.org/download/windows/'))
-			return $this->parse('/<h2>Current Version: ([\d\.]+)<\/h2>/');
+			return $this->parse('/<p>Current version ([\d\.]+)<\/p>/');
 		return false;
 	}
 }
