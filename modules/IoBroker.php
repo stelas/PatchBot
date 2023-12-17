b67431293624a32ceb9bf5b1ed2c82b57731a877
Steffen Lange <git@stelas.de>
2023-12-17 23:44:52 +0100

    Add ioBroker

---
+++ modules/IoBroker.php
@@ -0,0 +1,14 @@
+<?php
+
+class IoBroker extends PatchBase {
+	function __construct() {
+		parent::__construct('Denis Haev', 'ioBroker', 'https://www.iobroker.net/');
+	}
+	function check() : bool {
+		if ($this->fetch_json('https://api.github.com/repos/ioBroker/ioBroker/releases/latest'))
+			return $this->parse_json('tag_name');
+		return false;
+	}
+}
+
+?>
