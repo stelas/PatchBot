a6ee338a5f0d3e99dc41eff4d3fefd3ee5bccf76
Steffen Lange <git@stelas.de>
2023-12-27 12:11:52 +0100

    Chrome: use new JSON API

--- modules/GoogleChrome.php
+++ modules/GoogleChrome.php
@@ -5,8 +5,8 @@ class GoogleChrome extends PatchBase {
 		parent::__construct('Google', 'Chrome', 'https://www.google.de/chrome/');
 	}
 	function check() : bool {
-		if ($this->fetch('https://omahaproxy.appspot.com/all'))
-			return $this->parse('/win64,stable,([\d\.]+),/');
+		if ($this->fetch_json('https://versionhistory.googleapis.com/v1/chrome/platforms/win64/channels/stable/versions'))
+			return $this->parse_json('version');
 		return false;
 	}
 }
