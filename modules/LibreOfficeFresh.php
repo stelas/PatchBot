a0d037b98babd16c7624b7b9590ecbd12ee2cc45
Steffen Lange <git@stelas.de>
2024-02-12 18:13:09 +0100

    Request LibreOffice's check.php

--- modules/LibreOfficeFresh.php
+++ modules/LibreOfficeFresh.php
@@ -2,12 +2,12 @@
 
 class LibreOfficeFresh extends PatchBase {
 	function __construct() {
-		parent::__construct('Document Foundation', 'LibreOffice', 'https://www.libreoffice.org/download/download/');
+		parent::__construct('Document Foundation', 'LibreOffice', 'https://www.libreoffice.org/download/download-libreoffice/');
 		$this->patch->setBranch('Fresh');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.libreoffice.org/download/release-notes/'))
-			return $this->parse('/name="Fresh"><\/a>LibreOffice ([\d\.]+) \(/');
+		if ($this->fetch('https://update.libreoffice.org/check.php', array('CURLOPT_USERAGENT' => 'LibreOffice 24.2.0.1 (b4d45829793cddfe67b58a53f495528c75738d8a; Windows; X86_64; )')))
+			return $this->parse('/<inst:version>([\d\.]+)<\/inst:version>/');
 		return false;
 	}
 }
--- modules/LibreOfficeStill.php
+++ modules/LibreOfficeStill.php
@@ -2,12 +2,12 @@
 
 class LibreOfficeStill extends PatchBase {
 	function __construct() {
-		parent::__construct('Document Foundation', 'LibreOffice', 'https://www.libreoffice.org/download/download/');
+		parent::__construct('Document Foundation', 'LibreOffice', 'https://www.libreoffice.org/download/download-libreoffice/');
 		$this->patch->setBranch('Still');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.libreoffice.org/download/release-notes/'))
-			return $this->parse('/name="Still"><\/a>LibreOffice ([\d\.]+) \(/');
+		if ($this->fetch('https://update.libreoffice.org/check.php', array('CURLOPT_USERAGENT' => 'LibreOffice 7.6.3.2 (29d686fea9f6705b262d369fede658f824154cc0; Windows; X86_64; )')))
+			return $this->parse('/<inst:version>([\d\.]+)<\/inst:version>/');
 		return false;
 	}
 }
