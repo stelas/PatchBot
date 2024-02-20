57058539c73ec838ae6aefc2d0d3b6e59c79e19d
Steffen Lange <git@stelas.de>
2024-02-20 15:35:53 +0100

    Duplicity: parse Gitlab releases

--- modules/Duplicity.php
+++ modules/Duplicity.php
@@ -5,8 +5,8 @@ class Duplicity extends PatchBase {
 		parent::__construct('Kenneth Loafman', 'Duplicity', 'http://duplicity.gitlab.io/duplicity-web/');
 	}
 	function check() : bool {
-		if ($this->fetch_json('https://gitlab.com/api/v4/projects/12450835/repository/tags'))
-			return $this->parse_json('name', '/rel\.(.+)/');
+		if ($this->fetch_json('https://gitlab.com/api/v4/projects/12450835/releases'))
+			return $this->parse_json('tag_name', '/rel\.(.+)/');
 		return false;
 	}
 }
