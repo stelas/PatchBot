9d42d9682a05df808f482d4d5be74ae5102ee879
Steffen Lange <git@stelas.de>
2024-04-22 18:31:09 +0200

    Scribus: parse response header

--- modules/Scribus.php
+++ modules/Scribus.php
@@ -2,11 +2,11 @@
 
 class Scribus extends PatchBase {
 	function __construct() {
-		parent::__construct('Scribus Team', 'Scribus', 'https://www.scribus.net/downloads/stable-branch/');
+		parent::__construct('Scribus Team', 'Scribus', 'https://www.scribus.net/downloads/');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.scribus.net/downloads/'))
-			return $this->parse('_//sourceforge\.net/projects/scribus/files/scribus/([\d\.]+)/_');
+		if ($this->fetch_json('https://sourceforge.net/projects/scribus/best_release.json'))
+			return $this->parse_json('filename', '/^\/scribus\/(.+)\//');
 		return false;
 	}
 }
