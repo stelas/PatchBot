a2665facaf306abdcf3503f326fd10a6450ac755
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-04-23 15:06:32 +1200

    Alter ESPHome Author

--- modules/ESPHome.php
+++ modules/ESPHome.php
@@ -2,7 +2,7 @@
 
 class ESPHome extends PatchBase {
 	function __construct() {
-		parent::__construct('Jesse Hills', 'ESPHome', 'https://esphome.io/');
+		parent::__construct('ESPHome Developers', 'ESPHome', 'https://esphome.io/');
 	}
 	function check() : bool {
 		if ($this->fetch_json('https://api.github.com/repos/esphome/esphome/releases/latest'))
