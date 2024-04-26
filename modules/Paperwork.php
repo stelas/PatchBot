e3213a5eb9a56b15f8055fa0117c7222e0367aee
Steffen Lange <git@stelas.de>
2024-04-26 23:00:12 +0200

    Paperwork: use git tags

--- modules/Paperwork.php
+++ modules/Paperwork.php
@@ -5,8 +5,8 @@ class Paperwork extends PatchBase {
 		parent::__construct('Jérôme Flesch', 'Paperwork', 'https://openpaper.work/');
 	}
 	function check() : bool {
-		if ($this->fetch_json('https://gitlab.gnome.org/api/v4/projects/World%2FOpenPaperwork%2Fpaperwork/releases'))
-			return $this->parse_json('tag_name');
+		if ($this->fetch_json('https://gitlab.gnome.org/api/v4/projects/3560/repository/tags'))
+			return $this->parse_json('name');
 		return false;
 	}
 }
