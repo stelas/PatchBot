81687b2adfff2103598d9961d89e0cac65e1f1c9
Steffen Lange <git@stelas.de>
2024-05-31 19:08:54 +0200

    7-Zip: use Github api

--- modules/SevenZip.php
+++ modules/SevenZip.php
@@ -5,8 +5,8 @@ class SevenZip extends PatchBase {
 		parent::__construct('Igor Pavlov', '7-Zip', 'https://www.7-zip.org/download.html');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.7-zip.org/download.html'))
-			return $this->parse('/Download 7-Zip ([\d\.]+) \([\d-]+\)/');
+		if ($this->fetch_json('https://api.github.com/repos/ip7z/7zip/releases/latest'))
+			return $this->parse_json('tag_name');
 		return false;
 	}
 }
