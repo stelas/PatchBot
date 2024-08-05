f20a93d8db441fb1205bb3fdb9250de3b39c53df
Steffen Lange <git@stelas.de>
2024-08-05 12:13:59 +0200

    Add GCC 14

--- modules/GCC14.php
+++ modules/GCC14.php
@@ -1,13 +1,13 @@
 <?php
 
-class GCC11 extends PatchBase {
+class GCC14 extends PatchBase {
 	function __construct() {
 		parent::__construct('Free Software Foundation', 'GNU Compiler Collection', 'https://gcc.gnu.org/');
-		$this->patch->setBranch('11');
+		$this->patch->setBranch('14');
 	}
 	function check() : bool {
 		if ($this->fetch('https://gcc.gnu.org/'))
-			return $this->parse('/<span class="version"><a href="gcc-11\/">GCC ([\d\.]+)/');
+			return $this->parse('/<span class="version"><a href="gcc-14\/">GCC ([\d\.]+)/');
 		return false;
 	}
 }
