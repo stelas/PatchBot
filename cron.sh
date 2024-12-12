b9659dbd50ace1d88393bd7c86810081dbe92c96
Steffen Lange <git@stelas.de>
2024-12-12 14:12:22 +0100

    Remove code for tweeting

--- cron.sh
+++ cron.sh
@@ -6,6 +6,5 @@ SITEDIR="/var/www/virtual/steffen/patchbot.de"
 "$SCRIPTDIR/PatchCollector.php" || exit 1
 "$SCRIPTDIR/PatchViewer.php" > "$SITEDIR/index.html"
 "$SCRIPTDIR/PatchFeeder.php" > "$SITEDIR/rss.xml"
-#[ -e "$SCRIPTDIR/TwitterKey.php" ]  && "$SCRIPTDIR/PatchTwitter.php"
 [ -e "$SCRIPTDIR/MastodonKey.php" ] && "$SCRIPTDIR/PatchMastodon.php"
 
