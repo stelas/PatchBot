327502def7f4e44176e0fb075dab945541d17745
Steffen Lange <git@stelas.de>
2025-04-17 14:32:07 +0200

    Zoom: link to MSI packages

--- modules/Zoom.php
+++ modules/Zoom.php
@@ -2,7 +2,7 @@
 
 class Zoom extends PatchBase {
 	function __construct() {
-		parent::__construct('Zoom Video Communications', 'Zoom', 'https://zoom.us/download');
+		parent::__construct('Zoom Video Communications', 'Zoom', 'https://zoom.us/download/admin');
 	}
 	function check() : bool {
 		if ($this->fetch_header('https://zoom.us/client/latest/ZoomInstaller.exe'))
