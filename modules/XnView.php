09b570975cf2ea3bdb4fa35b34efee8d55da662d
Steffen Lange <git@stelas.de>
2025-06-19 17:44:17 +0200

    XnView: update URL

--- modules/XnView.php
+++ modules/XnView.php
@@ -2,10 +2,10 @@
 
 class XnView extends PatchBase {
 	function __construct() {
-		parent::__construct('Pierre-Emmanuel Gougelet', 'XnView', 'https://www.xnview.com/en/xnview/');
+		parent::__construct('Pierre-Emmanuel Gougelet', 'XnView', 'https://www.xnview.com/en/xnview-classic/');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.xnview.com/en/xnview/'))
+		if ($this->fetch('https://www.xnview.com/en/xnview-classic/'))
 			return $this->parse('/<strong>XnView ([\d\.]+)<\/strong>/');
 		return false;
 	}
