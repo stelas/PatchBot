b38c8b8ff052d4d688987048ee6786d216636b1c
Steffen Lange <steffen@lange.tel>
2025-06-21 17:18:57 +0200

    OpenVPN: use git releases

--- modules/OpenVPN.php
+++ modules/OpenVPN.php
@@ -5,8 +5,8 @@ class OpenVPN extends PatchBase {
 		parent::__construct('OpenVPN Inc.', 'OpenVPN', 'https://openvpn.net/community-downloads/');
 	}
 	function check() : bool {
-		if ($this->fetch_json('https://api.github.com/repos/OpenVPN/openvpn/tags'))
-			return $this->parse_json('name');
+		if ($this->fetch_json('https://api.github.com/repos/OpenVPN/openvpn/releases/latest'))
+			return $this->parse_json('tag_name');
 		return false;
 	}
 }
