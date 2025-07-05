20771449113c9d9552f8052ddaec44966239b063
Steffen Lange <steffen@lange.tel>
2025-07-05 15:38:12 +0200

    Tftpd64: fetch github api

--- modules/Tftpd32.php
+++ modules/Tftpd32.php
@@ -2,11 +2,11 @@
 
 class Tftpd32 extends PatchBase {
 	function __construct() {
-		parent::__construct('Philippe Jounin', 'Tftpd64', 'https://tftpd32.jounin.net/tftpd32_download.html');
+		parent::__construct('Philippe Jounin', 'Tftpd64', 'http://www.tftpd64.com/');
 	}
 	function check() : bool {
-		if ($this->fetch('https://pjo2.github.io/tftpd64/'))
-			return $this->parse('_/[Tt]ftpd64-([\d\.]+)-setup\.exe_');
+		if ($this->fetch_json('https://api.github.com/repos/PJO2/tftpd64/releases/latest'))
+			return $this->parse_json('tag_name');
 		return false;
 	}
 }
