b0a76ac120e67d23dee75e778510ee1590db3243
Steffen Lange <steffen@lange.tel>
2025-08-04 17:16:02 +0200

    FileZilla: imitate update search as workaround

--- modules/FileZilla.php
+++ modules/FileZilla.php
@@ -5,8 +5,8 @@ class FileZilla extends PatchBase {
 		parent::__construct('Tim Kosse', 'FileZilla', 'https://filezilla-project.org/download.php?show_all=1');
 	}
 	function check() : bool {
-		if ($this->fetch('https://filezilla-project.org/download.php?show_all=1'))
-			return $this->parse('/<p>The latest stable version of FileZilla Client is ([\d\.]+)<\/p>/');
+		if ($this->fetch('https://update.filezilla-project.org/update.php?initial=0&manual=1&osarch=64&osversion=10.0&package=1&platform=x86_64-w64-mingw32&updated=0&version=3.69.0', array('CURLOPT_USERAGENT' => 'FileZilla/3.69.0', 'CURLOPT_HTTPHEADER' => array('Accept:', 'Connection: close'), 'CURLOPT_SSL_VERIFYPEER' => false)))
+			return $this->parse('/release ([\d\.]+)/');
 		return false;
 	}
 }
