9aa9aa42b329c11cf73168d040a96f9751ae685c
Steffen Lange <steffen@lange.tel>
2025-10-16 17:35:33 +0200

    Scilab: fix regex

--- modules/Scilab.php
+++ modules/Scilab.php
@@ -5,8 +5,8 @@ class Scilab extends PatchBase {
 		parent::__construct('ESI Group', 'Scilab', 'https://www.scilab.org/download');
 	}
 	function check() : bool {
-		if ($this->fetch_header('https://www.scilab.org/latest'))
-			return $this->parse('/\/download\/scilab-([\d\.]+)/');
+		if ($this->fetch('https://www.scilab.org/download'))
+			return $this->parse('/<h1>Scilab ([\d\.]+)<\/h1>/');
 		return false;
 	}
 }
