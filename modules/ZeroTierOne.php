dbada0b23550d81fe9bc9e6974e54e7e23a6c0b0
Steffen Lange <steffen@lange.tel>
2026-01-15 14:19:40 +0100

    ZeroTierOne: fix product name

--- modules/ZeroTierOne.php
+++ modules/ZeroTierOne.php
@@ -2,7 +2,7 @@
 
 class ZeroTierOne extends PatchBase {
 	function __construct() {
-		parent::__construct('ZeroTier Inc.', 'ZeroTier', 'https://www.zerotier.com/download/');
+		parent::__construct('ZeroTier Inc.', 'ZeroTier One', 'https://www.zerotier.com/download/');
 	}
 	function check() : bool {
 		if ($this->fetch_json('https://api.github.com/repos/zerotier/ZeroTierOne/releases/latest'))
