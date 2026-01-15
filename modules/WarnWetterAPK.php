cd77db8b861f19e200a3c4e4bf598be763e96f01
Steffen Lange <steffen@lange.tel>
2026-01-15 14:23:15 +0100

    Warnwatter: rename scraper

--- modules/WarnWetterAPK.php
+++ modules/WarnWetterAPK.php
@@ -1,6 +1,6 @@
 <?php
 
-class WarnWetter extends PatchBase {
+class WarnWetterAPK extends PatchBase {
 	function __construct() {
 		parent::__construct('Deutscher Wetterdienst', 'WarnWetter', 'https://www.dwd.de/DE/leistungen/warnwetterapp/apk/download_apk_android5_x.html');
 		$this->patch->setBranch('Android');
