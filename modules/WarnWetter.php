47462ef3d2a40e4d5c4299e36b494df75918d7bd
Steffen Lange <steffen@lange.tel>
2026-01-15 14:20:01 +0100

    Add Warnwetter

---
+++ modules/WarnWetter.php
@@ -0,0 +1,15 @@
+<?php
+
+class WarnWetter extends PatchBase {
+	function __construct() {
+		parent::__construct('Deutscher Wetterdienst', 'WarnWetter', 'https://www.dwd.de/DE/leistungen/warnwetterapp/apk/download_apk_android5_x.html');
+		$this->patch->setBranch('Android');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.dwd.de/DE/leistungen/warnwetterapp/apk/download_apk_android5_x.html'))
+			return $this->parse('/\(apk - ([\d\.]+)\)/');
+		return false;
+	}
+}
+
+?>
