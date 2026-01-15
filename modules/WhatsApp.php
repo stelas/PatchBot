874fd1e9e8665f6d14d8b557541cb103558d830c
Steffen Lange <steffen@lange.tel>
2026-01-15 14:20:48 +0100

    Add WhatsApp

---
+++ modules/WhatsApp.php
@@ -0,0 +1,15 @@
+<?php
+
+class WhatsApp extends PatchBase {
+	function __construct() {
+		parent::__construct('WhatsApp LLC', 'WhatsApp', 'https://www.whatsapp.com/android');
+		$this->patch->setBranch('Android');
+	}
+	function check() : bool {
+		if ($this->fetch('https://www.whatsapp.com/android'))
+			return $this->parse('/\(Version ([\d\.]+)\)/');
+		return false;
+	}
+}
+
+?>
