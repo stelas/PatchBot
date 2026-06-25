082e02075d68fabbf11456007c6c81f65a56b28e
Steffen Lange <steffen@lange.tel>
2026-06-25 16:07:34 +0200

    BCompare5: add specific URL parameters

--- modules/BCompare5.php
+++ modules/BCompare5.php
@@ -6,7 +6,7 @@ class BCompare5 extends PatchBase {
 		$this->patch->setBranch('5');
 	}
 	function check() : bool {
-		if ($this->fetch('https://www.scootersoftware.com/checkupdates.php?product=bc5'))
+		if ($this->fetch('https://www.scootersoftware.com/checkupdates.php?product=bc5&minor=1&maint=7&auth=30.15&build=31736&edition=prodebug&cpuarch=x86_64&platform=win32&lang=silentC'))
 			return $this->parse('/Version <b>([\d\.]+ build \d+)<\/b>/');
 		return false;
 	}
