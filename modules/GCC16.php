b5903f4d2e84ffc3d2d64937e2baad096c8d3ef5
Steffen Lange <steffen@lange.tel>
2026-06-25 16:04:32 +0200

    Add GNU Compiler Collection 16

---
+++ modules/GCC16.php
@@ -0,0 +1,15 @@
+<?php
+
+class GCC16 extends PatchBase {
+	function __construct() {
+		parent::__construct('Free Software Foundation', 'GNU Compiler Collection', 'https://gcc.gnu.org/');
+		$this->patch->setBranch('16');
+	}
+	function check() : bool {
+		if ($this->fetch('https://gcc.gnu.org/'))
+			return $this->parse('/<span class="version"><a href="gcc-16\/">GCC ([\d\.]+)/');
+		return false;
+	}
+}
+
+?>
